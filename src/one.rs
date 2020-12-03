cf183d351619c169ec9db46cab9fb52b66025241
Christopher Wells <cwellsny@nycap.rr.com>
2020-12-02 20:58:53 -0800

    Solve day1 part two

--- src/one.rs
+++ src/one.rs
@@ -9,6 +9,10 @@ pub fn part_one() {
 }
 
 pub fn part_two() {
+    let numbers = read_input();
+    let answer = multiply_2020_three_addends(&numbers);
+
+    println!("{}", answer);
 }
 
 fn read_input() -> Vec<i32> {
@@ -32,6 +36,28 @@ fn multiply_2020_addends(numbers: &mut Vec<i32>) -> i32 {
 
         if numbers.binary_search(&target).is_ok() {
             return *num * target;
+        }
+    }
+
+    panic!("No valid answer.");
+}
+
+fn multiply_2020_three_addends(numbers: &Vec<i32>) -> i32 {
+    let size = numbers.len();
+
+    for i in 0..size {
+        for j in 0..size {
+            for k in 0..size {
+                if i != j && i != k && j != k {
+                    let a = numbers[i];
+                    let b = numbers[j];
+                    let c = numbers[k];
+
+                    if a + b + c == 2020 {
+                        return a * b * c;
+                    }
+                }
+            }
         }
     }
 
