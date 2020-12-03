65020e574754e9ebfa08fdd4f003298ed2d45813
Christopher Wells <cwellsny@nycap.rr.com>
2020-12-02 21:52:57 -0800

    Solve day2 part one

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,13 @@ fn main() {
                     .index(1)
                 )
             )
+            .subcommand(SubCommand::with_name("day2")
+                .arg(Arg::with_name("part")
+                    .help("Selects the part to run (one, two)")
+                    .required(true)
+                    .index(1)
+                )
+            )
         .get_matches()
     ;
 
@@ -23,6 +30,14 @@ fn main() {
         match part {
             "one" => { advent_of_code_2020::one::part_one(); }
             "two" => { advent_of_code_2020::one::part_two(); }
+            p => { println!("Unknown part: {}", p); }
+        }
+    } else if let Some(matches) = matches.subcommand_matches("day2") {
+        let part = matches.value_of("part").unwrap();
+
+        match part {
+            "one" => { advent_of_code_2020::two::part_one(); }
+            "two" => { advent_of_code_2020::two::part_two(); }
             p => { println!("Unknown part: {}", p); }
         }
     }
---
+++ src/two.rs
@@ -0,0 +1,71 @@
+
+use std::io;
+use std::io::prelude::*;
+
+pub fn part_one() {
+    let cases = read_input();
+    let answer = count_invalid_passwords(&cases);
+
+    println!("{}", answer);
+}
+
+pub fn part_two() {
+}
+
+pub struct PasswordRule {
+    min: i32,
+    max: i32,
+    letter: char,
+}
+
+impl PasswordRule {
+    pub fn parse_rule_and_password(line: &str) -> (PasswordRule, String) {
+        let parts: Vec<&str> = line.split(' ').collect();
+
+        assert!(parts.len() == 3);
+
+        let min_max: Vec<&str> = parts[0].split('-').collect();
+        let letter = parts[1].chars().next().unwrap();
+        let password = parts[2];
+
+        let min: i32 = min_max[0].parse().unwrap();
+        let max: i32 = min_max[1].parse().unwrap();
+
+        let rule = PasswordRule {
+            min, max, letter
+        };
+
+        (rule, password.to_string())
+    }
+
+    pub fn validate(&self, password: &str) -> bool {
+        let letter_count = password.chars()
+            .filter(|l| *l == self.letter)
+            .count() as i32;
+
+        self.min <= letter_count && letter_count <= self.max
+    }
+}
+
+fn read_input() -> Vec<(PasswordRule, String)> {
+    let stdin = io::stdin();
+
+    let mut rules_and_passwords: Vec<(PasswordRule, String)> = Vec::new();
+    for line in stdin.lock().lines() {
+        let line = line.unwrap();
+        let rule_and_password = PasswordRule::parse_rule_and_password(&line);
+
+        rules_and_passwords.push(rule_and_password);
+    }
+
+    rules_and_passwords
+}
+
+fn count_invalid_passwords(rules_and_passwords: &Vec<(PasswordRule, String)>) -> i32 {
+    rules_and_passwords.iter()
+        .filter(|rule_and_password| {
+            let (rule, password) = rule_and_password;
+
+            rule.validate(password)
+        }).count() as i32
+}
