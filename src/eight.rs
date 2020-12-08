546d328cafee3d5c258c6f5b2fcf23368c30c231
Christopher Wells <cwellsny@nycap.rr.com>
2020-12-07 23:48:14 -0800

    Solve day8 part two

--- src/eight.rs
+++ src/eight.rs
@@ -4,17 +4,38 @@ use std::io::{self, BufRead};
 pub fn part_one() {
     let program = read_input(io::stdin().lock());
     let answer = program
-        .run_until_inifnite_loop(&ProgramState {
+        .run(&ProgramState {
             current_instruction: 0,
             accumulator_value: 0,
         })
+        .get_state()
         .accumulator_value;
 
     println!("{}", answer)
 }
 
-pub fn part_two() {}
+pub fn part_two() {
+    let program = read_input(io::stdin().lock());
+    let answer = get_terminating_accumulator_value_after_fix(&program).unwrap();
 
+    println!("{}", answer)
+}
+
+enum ProgramResult {
+    InfiniteLoop(ProgramState),
+    Terminated(ProgramState),
+}
+
+impl ProgramResult {
+    fn get_state(&self) -> &ProgramState {
+        match self {
+            ProgramResult::InfiniteLoop(state) => state,
+            ProgramResult::Terminated(state) => state,
+        }
+    }
+}
+
+#[derive(Clone)]
 struct Program {
     instructions: Vec<Instruction>,
 }
@@ -26,20 +47,24 @@ impl Program {
         }
     }
 
-    fn run_until_inifnite_loop(&self, intial_state: &ProgramState) -> ProgramState {
+    fn run(&self, intial_state: &ProgramState) -> ProgramResult {
         let mut state = intial_state.clone();
 
         let mut visited_lines: BTreeSet<usize> = BTreeSet::new();
         loop {
             if visited_lines.contains(&state.current_instruction) {
-                break;
+                return ProgramResult::InfiniteLoop(state);
+            } else if state.current_instruction == self.instructions.len() {
+                return ProgramResult::Terminated(state);
             }
             visited_lines.insert(state.current_instruction);
 
             state.execute(&self.instructions[state.current_instruction]);
         }
+    }
 
-        state.clone()
+    fn change_instruction(&mut self, index: usize, fun: &dyn Fn(&Instruction) -> Instruction) {
+        self.instructions[index] = fun(&self.instructions[index]);
     }
 }
 
@@ -52,7 +77,7 @@ struct ProgramState {
 impl ProgramState {
     fn execute(&mut self, instruction: &Instruction) {
         match instruction {
-            Instruction::Nop() => self.current_instruction += 1,
+            Instruction::Nop(_) => self.current_instruction += 1,
             Instruction::Acc(change) => {
                 self.current_instruction += 1;
                 self.accumulator_value += change;
@@ -64,10 +89,11 @@ impl ProgramState {
     }
 }
 
+#[derive(Clone)]
 enum Instruction {
     Acc(i32),
     Jmp(i32),
-    Nop(),
+    Nop(i32),
 }
 
 impl Instruction {
@@ -77,7 +103,7 @@ impl Instruction {
         assert!(parts.len() == 2);
 
         match parts[0] {
-            "nop" => Instruction::Nop(),
+            "nop" => Instruction::Nop(parts[1].parse().unwrap()),
             "acc" => Instruction::Acc(parts[1].parse().unwrap()),
             "jmp" => Instruction::Jmp(parts[1].parse().unwrap()),
             _ => panic!(),
@@ -90,4 +116,45 @@ where
     R: BufRead,
 {
     Program::from_lines(&reader.lines().map(|l| l.unwrap().to_string()).collect())
+}
+
+fn get_terminating_accumulator_value_after_fix(program: &Program) -> Option<i32> {
+    let nop_and_jmp_indices: Vec<usize> = program
+        .instructions
+        .iter()
+        .enumerate()
+        .filter(|(_, inst)| match inst {
+            Instruction::Nop(_) => true,
+            Instruction::Jmp(_) => true,
+            Instruction::Acc(_) => false,
+        })
+        .map(|(index, _)| index)
+        .collect();
+
+    let mut new_program = program.clone();
+    for index in nop_and_jmp_indices.iter() {
+        new_program.change_instruction(*index, &switch_nop_or_jmp);
+
+        let results = new_program.run(&ProgramState {
+            current_instruction: 0,
+            accumulator_value: 0,
+        });
+
+        match results {
+            ProgramResult::InfiniteLoop(_) => (),
+            ProgramResult::Terminated(state) => return Some(state.accumulator_value),
+        }
+
+        new_program.change_instruction(*index, &switch_nop_or_jmp);
+    }
+
+    None
+}
+
+fn switch_nop_or_jmp(instruction: &Instruction) -> Instruction {
+    match instruction {
+        Instruction::Nop(n) => Instruction::Jmp(*n),
+        Instruction::Jmp(n) => Instruction::Nop(*n),
+        Instruction::Acc(_) => panic!(),
+    }
 }
