3313df19adb52dcd665e1fb9729d563ec9c4e218
Christopher Wells <cwellsny@nycap.rr.com>
2020-12-10 00:18:55 -0800

    Solve day10 part two

--- src/ten.rs
+++ src/ten.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeSet;
+use std::collections::{BTreeMap, BTreeSet};
 use std::io::{self, BufRead};
 
 pub fn part_one() {
@@ -8,7 +8,12 @@ pub fn part_one() {
     println!("{}", answer)
 }
 
-pub fn part_two() {}
+pub fn part_two() {
+    let adapters = read_input(io::stdin().lock());
+    let answer = get_total_num_valid_adapter_configurations(&adapters);
+
+    println!("{}", answer)
+}
 
 fn read_input<R>(reader: R) -> BTreeSet<i32>
 where
@@ -35,4 +40,56 @@ fn get_product_of_num_1_diffs_and_num_3_diffs(adapters: &BTreeSet<i32>) -> i32 {
     let num_3_diffs = diffs.iter().filter(|a| **a == 3).count() as i32;
 
     num_1_diffs * num_3_diffs
+}
+
+fn get_total_num_valid_adapter_configurations(adapters: &BTreeSet<i32>) -> u64 {
+    let device = adapters.iter().max().unwrap() + 3;
+
+    let mut adapters_wall_and_device = adapters.clone();
+    adapters_wall_and_device.insert(0);
+    adapters_wall_and_device.insert(device);
+
+    calc_num_valid_paths(&adapters_wall_and_device, 0, device, &mut BTreeMap::new())
+}
+
+fn get_next_nodes(nodes: &BTreeSet<i32>, node: i32) -> Option<Vec<i32>> {
+    let max = *nodes.iter().max().unwrap();
+
+    if node == max {
+        None
+    } else {
+        Some(
+            (1..4)
+                .map(|i| i + node)
+                .filter(|n| nodes.contains(n))
+                .collect(),
+        )
+    }
+}
+
+fn calc_num_valid_paths(
+    nodes: &BTreeSet<i32>,
+    source: i32,
+    destination: i32,
+    cache: &mut BTreeMap<i32, u64>,
+) -> u64 {
+    if source == destination {
+        1
+    } else if cache.contains_key(&source) {
+        *cache.get(&source).unwrap()
+    } else {
+        match get_next_nodes(nodes, source) {
+            None => 0,
+            Some(next_nodes) => {
+                let num_paths = next_nodes
+                    .iter()
+                    .map(|n| calc_num_valid_paths(nodes, *n, destination, cache))
+                    .sum();
+
+                cache.insert(source, num_paths);
+
+                num_paths
+            }
+        }
+    }
 }
