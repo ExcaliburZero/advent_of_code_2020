fc93794f08f253c23398867a7cb57d9254a7f04a
Christopher Wells <cwellsny@nycap.rr.com>
2020-12-13 23:25:01 -0800

    Solve day11 part two

--- src/eleven.rs
+++ src/eleven.rs
@@ -6,11 +6,20 @@ use eleven::itertools::Itertools;
 
 pub fn part_one() {
     let seats = read_input(io::stdin().lock());
-    let answer = num_occupied_seats_once_stable(&seats);
+    let answer = num_occupied_seats_once_stable(&seats, &IterationRule::DirectNeighbors);
     println!("{}", answer);
 }
 
-pub fn part_two() {}
+pub fn part_two() {
+    let seats = read_input(io::stdin().lock());
+    let answer = num_occupied_seats_once_stable(&seats, &IterationRule::LinesOfSight);
+    println!("{}", answer);
+}
+
+enum IterationRule {
+    DirectNeighbors,
+    LinesOfSight,
+}
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 enum SeatStatus {
@@ -100,8 +109,47 @@ impl Seats {
 
         neighbor_positions
             .iter()
-            .flat_map(|p| self.get(p).map(|v| (p.clone(), v)))
+            .flat_map(|p| self.get(p).map(|v| (*p, v)))
             .collect()
+    }
+
+    fn get_line_of_sight_neighbors(&self, center: &Position) -> Vec<(Position, SeatStatus)> {
+        let mut directions: Vec<(i32, i32)> = vec![];
+        for rows in -1..=1 {
+            for columns in -1..=1 {
+                if rows != 0 || columns != 0 {
+                    directions.push((rows, columns));
+                }
+            }
+        }
+
+        let mut line_of_sight_neighbors: Vec<(Position, SeatStatus)> = vec![];
+        for dir in directions {
+            let mut seat_in_dir = Some(*center);
+            let mut found_seat = false;
+            while !found_seat && seat_in_dir.is_some() {
+                seat_in_dir = seat_in_dir.unwrap().add(dir.0, dir.1);
+
+                match seat_in_dir {
+                    None => (),
+                    Some(ref seat_pos) => match self.get(&seat_pos) {
+                        None => seat_in_dir = None,
+                        Some(SeatStatus::Floor) => (),
+                        Some(SeatStatus::Empty) => {
+                            found_seat = true;
+                            line_of_sight_neighbors.push((seat_in_dir.unwrap(), SeatStatus::Empty));
+                        }
+                        Some(SeatStatus::Occupied) => {
+                            found_seat = true;
+                            line_of_sight_neighbors
+                                .push((seat_in_dir.unwrap(), SeatStatus::Occupied));
+                        }
+                    },
+                }
+            }
+        }
+
+        line_of_sight_neighbors
     }
 
     fn num_occupied_neighbors(&self, pos: &Position) -> u32 {
@@ -112,25 +160,52 @@ impl Seats {
             .count() as u32
     }
 
-    fn seat_next_value(&self, pos: &Position) -> SeatStatus {
+    fn num_occupied_line_of_sights(&self, pos: &Position) -> u32 {
+        self.get_line_of_sight_neighbors(pos)
+            .iter()
+            .map(|(_, status)| status)
+            .filter(|status| **status == SeatStatus::Occupied)
+            .count() as u32
+    }
+
+    fn seat_next_value(&self, pos: &Position, iteration_rule: &IterationRule) -> SeatStatus {
         match self.get(pos) {
             None => panic!(),
-            Some(v) => match v {
-                SeatStatus::Floor => SeatStatus::Floor,
-                SeatStatus::Empty => {
-                    if self.num_occupied_neighbors(pos) == 0 {
-                        SeatStatus::Occupied
-                    } else {
-                        SeatStatus::Empty
+            Some(v) => match iteration_rule {
+                IterationRule::DirectNeighbors => match v {
+                    SeatStatus::Floor => SeatStatus::Floor,
+                    SeatStatus::Empty => {
+                        if self.num_occupied_neighbors(pos) == 0 {
+                            SeatStatus::Occupied
+                        } else {
+                            SeatStatus::Empty
+                        }
                     }
-                }
-                SeatStatus::Occupied => {
-                    if self.num_occupied_neighbors(pos) >= 4 {
-                        SeatStatus::Empty
-                    } else {
-                        SeatStatus::Occupied
+                    SeatStatus::Occupied => {
+                        if self.num_occupied_neighbors(pos) >= 4 {
+                            SeatStatus::Empty
+                        } else {
+                            SeatStatus::Occupied
+                        }
                     }
-                }
+                },
+                IterationRule::LinesOfSight => match v {
+                    SeatStatus::Floor => SeatStatus::Floor,
+                    SeatStatus::Empty => {
+                        if self.num_occupied_line_of_sights(pos) == 0 {
+                            SeatStatus::Occupied
+                        } else {
+                            SeatStatus::Empty
+                        }
+                    }
+                    SeatStatus::Occupied => {
+                        if self.num_occupied_line_of_sights(pos) >= 5 {
+                            SeatStatus::Empty
+                        } else {
+                            SeatStatus::Occupied
+                        }
+                    }
+                },
             },
         }
     }
@@ -146,9 +221,9 @@ impl Seats {
         positions
     }
 
-    fn iteration(&self, output_seats: &mut Seats) {
+    fn iteration(&self, iteration_rule: &IterationRule, output_seats: &mut Seats) {
         for pos in self.positions() {
-            output_seats.set(&pos, self.seat_next_value(&pos));
+            output_seats.set(&pos, self.seat_next_value(&pos, iteration_rule));
         }
     }
 
@@ -178,7 +253,7 @@ impl Seats {
     }*/
 }
 
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 struct Position {
     row: usize,
     column: usize,
@@ -209,13 +284,13 @@ where
     Seats::from_lines(&lines)
 }
 
-fn num_occupied_seats_once_stable(seats: &Seats) -> usize {
+fn num_occupied_seats_once_stable(seats: &Seats, iteration_rule: &IterationRule) -> usize {
     let mut seats_a = seats.clone();
     let mut seats_b = seats.clone();
 
     let mut unstable = true;
     while unstable {
-        seats_a.iteration(&mut seats_b);
+        seats_a.iteration(iteration_rule, &mut seats_b);
 
         if seats_a == seats_b {
             unstable = false;
@@ -282,7 +357,7 @@ mod tests {
         assert_eq!(1, seats.num_columns);
 
         let mut next_seats = seats.clone();
-        seats.iteration(&mut next_seats);
+        seats.iteration(&IterationRule::DirectNeighbors, &mut next_seats);
 
         assert_eq!(Some(SeatStatus::Occupied), next_seats.get(&position(0, 0)));
     }
@@ -296,7 +371,7 @@ mod tests {
         assert_eq!(1, seats.num_columns);
 
         let mut next_seats = seats.clone();
-        seats.iteration(&mut next_seats);
+        seats.iteration(&IterationRule::DirectNeighbors, &mut next_seats);
 
         assert_eq!(Some(SeatStatus::Occupied), next_seats.get(&position(0, 0)));
     }
@@ -310,7 +385,7 @@ mod tests {
         assert_eq!(1, seats.num_columns);
 
         let mut next_seats = seats.clone();
-        seats.iteration(&mut next_seats);
+        seats.iteration(&IterationRule::DirectNeighbors, &mut next_seats);
 
         assert_eq!(Some(SeatStatus::Floor), next_seats.get(&position(0, 0)));
     }
@@ -332,7 +407,7 @@ mod tests {
         let seats = Seats::from_lines(&lines);
 
         let expected = 37;
-        let actual = num_occupied_seats_once_stable(&seats);
+        let actual = num_occupied_seats_once_stable(&seats, &IterationRule::DirectNeighbors);
 
         assert_eq!(expected, actual);
     }
